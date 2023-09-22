35a4fa316e75c526cf6147b0a3e789e1cc8da73a
Rafael Moreira <rafa.siffert@gmail.com>
2023-09-22 17:02:43 -0400

    gorilla profile does not render data from json

--- momo-family-database/src/components/GorillaProfile.js
+++ momo-family-database/src/components/GorillaProfile.js
@@ -4,25 +4,50 @@ import { Link, useParams } from 'react-router-dom';
 function GorillaProfile() {
   const { id } = useParams();
   const [gorilla, setGorilla] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Fetch the gorilla data using the `id` from the URL
-    fetch(`http://localhost:3001/momoFamily`)
-      .then((response) => response.json())
+    const gorillaId = parseInt(id, 10);
+
+    fetch('http://localhost:3001/momoFamily')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        return response.json();
+      })
       .then((data) => {
-        console.log('Data from API:', data); // Debug: Log the data
-        const foundGorilla = data.find((g) => g.ID === id); // Use `data` directly
-        console.log('Found Gorilla:', foundGorilla); // Debug: Log the found gorilla
-        setGorilla(foundGorilla);
+        console.log('Data from API:', data);
+
+        const foundGorilla = data.momoFamily.find((g) => g.ID === gorillaId);
+        console.log('Found Gorilla:', foundGorilla);
+
+        if (foundGorilla) {
+          setGorilla(foundGorilla);
+        } else {
+          setError(`Gorilla with ID ${gorillaId} not found.`);
+        }
+
+        setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
-        console.log('Error:', error); // Debug: Log the error
+        setError(error.message);
+        setLoading(false);
       });
   }, [id]);
 
-  if (!gorilla) {
+  if (loading) {
     return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!gorilla) {
+    return <div>Gorilla not found.</div>;
   }
 
   return (
--- momo-family-database/src/components/GorillaSelection.js
+++ momo-family-database/src/components/GorillaSelection.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MomotaroImage from '../Assets/images/GorillaSelection/Momotaro.png';
+import GenkiImage from '../Assets/images/GorillaSelection/Genki.png';
+import GentaroImage from '../Assets/images/GorillaSelection/Gentaro.png';
+import KintaroImage from '../Assets/images/GorillaSelection/Kintaro.png';
+import '../GorillaSelection.css';
 
 function GorillaSelection() {
-  // Placeholder gorillas for now
   const gorillas = [
-    { ID: '0079', Name: 'Genki' },
-    { ID: '0091', Name: 'Momotaro' },
-    { ID: '0096', Name: 'Gentaro' },
-    { ID: '0103', Name: 'Kintaro' },
+    { ID: '1', Name: 'Momotaro', Image: MomotaroImage },
+    { ID: '2', Name: 'Genki', Image: GenkiImage },
+    { ID: '3', Name: 'Gentaro', Image: GentaroImage },
+    { ID: '4', Name: 'Kintaro', Image: KintaroImage },
   ];
 
   return (
-    <div>
-      {gorillas.map((gorilla) => (
-        <Link key={gorilla.ID} to={`/momofamily/${gorilla.ID}`}>
-          <div>{gorilla.Name}</div>
-        </Link>
-      ))}
+    <div className="gorilla-container">
+      <div className="gorilla-gallery">
+        {gorillas.map((gorilla) => (
+          <Link key={gorilla.ID} to={`/momofamily/${gorilla.ID}`} className="gorilla-item">
+            <img src={gorilla.Image} alt={gorilla.Name} />
+            <div className="gorilla-name">{gorilla.Name}</div>
+          </Link>
+        ))}
+      </div>
       <Link to="/">
         <button>Back</button>
       </Link>
