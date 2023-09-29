9573f91e1fde315fd39d20cd4c46ff9423b16617
Rafael Moreira <rafa.siffert@gmail.com>
2023-09-28 22:11:02 -0400

    latest

--- momo-family-database/src/components/AppContent.jsx
+++ momo-family-database/src/components/AppContent.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import { TextureLoader } from 'three';
 import { Text as DreiText, OrbitControls } from '@react-three/drei';
 import { useLoader } from 'react-three-fiber';
+import { CubeTextureLoader } from 'three';
+
 
 function AppContent() {
   const [view, setView] = useState('landing'); // 'landing' or 'selection'
@@ -120,29 +122,43 @@ function GorillaSelectionContent({ setView }) {
       <ambientLight intensity={0.5} />
       <directionalLight intensity={0.8} position={[2, 2, 2]} />
       {gorillaImages.map((image, index) => (
-        <GorillaImage key={index} image={image} position={[-2 + index * 2, 0, 0]} />
+        <GorillaImage key={index} image={image} position={[-2.3 + index * 1.5, 0, 2.1]} rotation={[.1, 0, 0]} />
       ))}
       <DreiText
         position={[0, -2, 0]}
-        scale={[0.1, 0.1, 0.1]}
+        scale={[0.3, 0.3, 0.1]}
         color="white"
         onClick={() => setView('landing')}
       >
         Back
       </DreiText>
+      <OrbitControls />
     </>
   );
 }
 
-function GorillaImage({ image, position }) {
-  const texture = useLoader(TextureLoader, image);
-
-  return (
-    <mesh position={position}>
-      <planeGeometry args={[1, 1]} />
-      <meshBasicMaterial map={texture} />
-    </mesh>
-  );
-}
+function GorillaImage({ image, position, rotation }) {
+    // Use useLoader from react-three-fiber to load textures
+    const texture = useLoader(TextureLoader, image);
+  
+    // Enable alpha transparency for the material
+    texture.encoding = 16; // This is equivalent to THREE.sRGBEncoding
+    texture.anisotropy = 16; // Adjust the anisotropy value as needed
+  
+    // Create a material with alpha blending enabled
+    const material = {
+      map: texture,
+      transparent: true, // Enable transparency
+      alphaTest: 0.5, // Adjust the alphaTest value as needed
+    };
+  
+    return (
+      <mesh position={position} rotation={rotation}>
+        <planeGeometry args={[1.1, 1.2]} />
+        <meshBasicMaterial {...material} />
+      </mesh>
+    );
+  }
+  
 
 export default AppContent;
