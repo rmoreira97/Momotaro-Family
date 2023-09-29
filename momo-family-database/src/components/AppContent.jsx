67f12bade77df7828e3fea158393e5fa124e9c95
Rafael Moreira <rafa.siffert@gmail.com>
2023-09-28 21:30:21 -0400

    landing goes to selection

--- momo-family-database/src/components/AppContent.jsx
+++ momo-family-database/src/components/AppContent.jsx
@@ -1,76 +1,135 @@
-import React, { useState, Suspense } from 'react';
-import { Canvas, useLoader } from 'react-three-fiber';
+import React, { useState, useRef } from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
 import { useNavigate } from 'react-router-dom';
 import { TextureLoader } from 'three';
 import { Text as DreiText, OrbitControls } from '@react-three/drei';
-import CustomBackButton from './CustomBackButton'; // Assuming this is in the same directory
+import { useLoader } from 'react-three-fiber';
 
 function AppContent() {
   const [view, setView] = useState('landing'); // 'landing' or 'selection'
+  const cameraRef = useRef();
 
-  const handleExploreClick = () => {
-    setView('selection');
-  };
+  // Define the camera positions
+  const startCameraPosition = [0, 0, 5];
+  const endCameraPosition = [0, 0, 10];
+
+  // Define the animation parameters
+  const animationSpeed = 0.01;
+  const animationDirection = view === 'selection' ? 1 : -1;
 
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
-      <Canvas camera={{ position: [0, 0, 5] }}>
-        {view === 'landing' && <LandingPageContent onExploreClick={handleExploreClick} />}
-        {view === 'selection' && <GorillaSelectionContent />}
+      <Canvas>
+        <perspectiveCamera
+          makeDefault
+          position={startCameraPosition}
+          ref={cameraRef}
+        />
+        {view === 'landing' && <LandingPageContent onExploreClick={() => setView('selection')} />}
+        {view === 'selection' && <GorillaSelectionContent setView={() => setView('landing')} />}
+        {/* Move the useFrame hook inside the Canvas component */}
+        <CameraAnimation
+          cameraRef={cameraRef}
+          view={view}
+          setView={setView}
+          startCameraPosition={startCameraPosition}
+          endCameraPosition={endCameraPosition}
+          animationSpeed={animationSpeed}
+          animationDirection={animationDirection}
+        />
       </Canvas>
     </div>
   );
 }
 
-function LandingPageContent({ onExploreClick }) {
-    const familyTexture = useLoader(TextureLoader, '/Assets/images/family.png');
-  
-    return (
-      <>
-        <ambientLight intensity={0.2} />
-        <DreiText 
-          position={[0, 0.6, 3.3]} 
-          scale={[0.2, 0.2, 0.2]}
-          rotation={[0.8, 0, 0]}
-        > 
-          The Momotaro Family
-        </DreiText>
-        <sprite position={[0, 0.33, 3.6]} scale={[1, 1, 1]}>
-          <spriteMaterial attach="material" map={familyTexture} />
-        </sprite>
-        <DreiText
-  position={[0, -0.02, 4.6]}  // Adjusted the Y position to bring it closer to the center
-  scale={[0.1, 0.1, 0.1]}
-  rotation={[-1, 0, 0]}
-  color="white"  // Changed the color to red for visibility
-  onClick={onExploreClick}
->
-  Explore Now
-</DreiText>
-        <OrbitControls />
-      </>
-    );
-  }
+// Define CameraAnimation component
+function CameraAnimation({
+  cameraRef,
+  view,
+  setView,
+  startCameraPosition,
+  endCameraPosition,
+  animationSpeed,
+  animationDirection,
+}) {
+  useFrame(() => {
+    const camera = cameraRef.current;
 
-function GorillaSelectionContent() {
+    // Calculate the new position
+    const newPosition = camera.position.clone().addScaledVector(
+      endCameraPosition,
+      animationSpeed * animationDirection
+    );
+
+    // Set the new position
+    camera.position.copy(newPosition);
+
+    // Check if the animation is complete
+    if (
+      (animationDirection === 1 && camera.position.z >= endCameraPosition[2]) ||
+      (animationDirection === -1 && camera.position.z <= startCameraPosition[2])
+    ) {
+      setView(view === 'landing' ? 'selection' : 'landing');
+    }
+  });
+
+  return null;
+}
+
+function LandingPageContent({ onExploreClick }) {
+  const familyTexture = useLoader(TextureLoader, '/Assets/images/family.png');
+
+  return (
+    <>
+      <ambientLight intensity={0.2} />
+      <DreiText
+        position={[0, 0.6, 3.3]}
+        scale={[0.2, 0.2, 0.2]}
+        rotation={[0.8, 0, 0]}
+      >
+        The Momotaro Family
+      </DreiText>
+      <sprite position={[0, 0.33, 3.6]} scale={[1, 1, 1]}>
+        <spriteMaterial attach="material" map={familyTexture} />
+      </sprite>
+      <DreiText
+        position={[0, -0.02, 4.6]}
+        scale={[0.1, 0.1, 0.1]}
+        rotation={[-1, 0, 0]}
+        color="white"
+        onClick={onExploreClick}
+      >
+        Explore Now
+      </DreiText>
+      <OrbitControls />
+    </>
+  );
+}
+
+function GorillaSelectionContent({ setView }) {
   const navigate = useNavigate();
   const gorillaImages = [
     '/Assets/images/GorillaSelection/Momotaro.png',
     '/Assets/images/GorillaSelection/Genki.png',
     '/Assets/images/GorillaSelection/Gentaro.png',
-    '/Assets/images/GorillaSelection/Kintaro.png'
+    '/Assets/images/GorillaSelection/Kintaro.png',
   ];
 
   return (
     <>
       <ambientLight intensity={0.5} />
       <directionalLight intensity={0.8} position={[2, 2, 2]} />
-      <Suspense fallback={null}>
-        {gorillaImages.map((image, index) => (
-          <GorillaImage key={index} image={image} position={[-2 + index * 2, 0, 0]} />
-        ))}
-        <CustomBackButton onClick={() => navigate('/')} position={[0, -2, -2]} />
-      </Suspense>
+      {gorillaImages.map((image, index) => (
+        <GorillaImage key={index} image={image} position={[-2 + index * 2, 0, 0]} />
+      ))}
+      <DreiText
+        position={[0, -2, 0]}
+        scale={[0.1, 0.1, 0.1]}
+        color="white"
+        onClick={() => setView('landing')}
+      >
+        Back
+      </DreiText>
     </>
   );
 }
